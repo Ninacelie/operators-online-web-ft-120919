e653c57117817764dc8a65af1e0333f8cdd7ff90
Nina Angelo <ninacelie@gmail.com>
2019-12-10 14:06:32 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -11,12 +11,13 @@ end
 
 
 def not_safe?(speed)
-  if speed < 60
+  if speed > 60
     return true
-  elsif speed > 60
+  elsif speed < 40
     return true
   else
     return false
+    
   end
 
 end
